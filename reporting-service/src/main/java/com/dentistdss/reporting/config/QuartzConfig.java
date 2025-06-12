8423f9c5fa9636f5a5af7bd79ccb2f46a7c14339
Zhifei Mi <zm377@uowmail.edu.au>
2025-06-12 10:33:20 +1000

    feat: Add Quartz Scheduler configuration for primary DataSource; remove obsolete ReportingServiceTest

---
+++ reporting-service/src/main/java/com/dentistdss/reporting/config/QuartzConfig.java
@@ -0,0 +1,36 @@
+package com.dentistdss.reporting.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * Quartz Scheduler Configuration
+ * 
+ * Configures Quartz to use the primary DataSource for job persistence
+ * when multiple DataSources are present in the application.
+ *
+ * @author zhifeimi
+ * @email zm377@uowmail.edu.au
+ * @github https://github.com/zm377
+ */
+@Slf4j
+@Configuration
+public class QuartzConfig {
+
+    /**
+     * Configure Quartz to use the primary DataSource
+     * The @QuartzDataSource annotation tells Spring Boot which DataSource
+     * to use for Quartz when multiple DataSources are available.
+     */
+    @Bean
+    @QuartzDataSource
+    public DataSource quartzDataSource(@Qualifier("primaryDataSource") DataSource primaryDataSource) {
+        log.info("Configuring Quartz to use primary DataSource");
+        return primaryDataSource;
+    }
+}
